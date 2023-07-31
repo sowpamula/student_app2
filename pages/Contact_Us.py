d404409c3d27cc381dc7c434f94e7e36a4bdca96
sowjanya <sowjanyapamulapati55@gmail.com>
2023-07-31 15:04:24 -0700

    Developed the Contact Us page and Email functionality

---
+++ pages/Contact_Us.py
@@ -0,0 +1,23 @@
+import pandas
+import streamlit as st
+from send_email import send_email
+
+with st.form(key="email_forms"):
+
+    user_email = st.text_input("Your Email Address")
+    df = pandas.read_csv("topics.csv")
+    user_selection = st.selectbox(" What topic do you want to discuss?", df[:])
+    raw_message = st.text_area("Text")
+    message = f"""\
+    Subject: New email from {user_email}
+
+    From:{user_email}
+    Topic:{user_selection}
+    {raw_message}
+    """
+    button = st.form_submit_button("Submit")
+
+    print(button)
+    if button:
+        send_email(message)
+        st.info("Your email was sent successfully")
